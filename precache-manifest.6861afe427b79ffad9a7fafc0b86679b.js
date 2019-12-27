ad650280159c135fba4a5261b31161a540e1fd4e
johnnybolony <ctexyg@gmail.com>
2019-12-27 10:27:42 +0200

    Updates

---
+++ precache-manifest.6861afe427b79ffad9a7fafc0b86679b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b27a2198582cf5f5866284f405f668",
+    "url": "/hooks-todos/index.html"
+  },
+  {
+    "revision": "c8efd92b9a15e99fb3a4",
+    "url": "/hooks-todos/static/js/2.777ef5ec.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/hooks-todos/static/js/2.777ef5ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42ee28de9c32a836e0b6",
+    "url": "/hooks-todos/static/js/main.c00783f6.chunk.js"
+  },
+  {
+    "revision": "d808d8d6b50a949c6303",
+    "url": "/hooks-todos/static/js/runtime-main.fc5810ea.js"
+  }
+]);
